9be46edfffe275b65f35cd438359d434fb3de6f8
kawasima <kawasima1016@gmail.com>
2015-01-27 21:48:57 +0900

    Change the remote endpoint to use the async endpoint

--- src/main/java/net/unit8/logback/WebSocketAppender.java
+++ src/main/java/net/unit8/logback/WebSocketAppender.java
@@ -13,10 +13,6 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.net.URI;
 import java.nio.ByteBuffer;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.Future;
-import java.util.concurrent.SynchronousQueue;
 
 /**
  * @author kawasima
@@ -26,10 +22,7 @@ public class WebSocketAppender extends AppenderBase<ILoggingEvent> {
             new LoggingEventPreSerializationTransformer();
 
     private URI serverUri;
-    private int queueSize = 0;
     private Session session;
-    private BlockingQueue<ILoggingEvent> queue;
-    private Future<?> task;
 
     private void started() {
         super.start();
@@ -38,7 +31,6 @@ public class WebSocketAppender extends AppenderBase<ILoggingEvent> {
     @Override
     public void start() {
         if (isStarted()) return;
-        final WebSocketAppender self = this;
 
         WebSocketContainer container = ContainerProvider.getWebSocketContainer();
         try {
@@ -50,25 +42,8 @@ public class WebSocketAppender extends AppenderBase<ILoggingEvent> {
 
                 @Override
                 public void onClose(Session session, CloseReason closeReason) {
-                    System.out.println("close log: " + closeReason);
                 }
             }, ClientEndpointConfig.Builder.create().build(), serverUri);
-            queue = newBlockingQueue(queueSize);
-            task = self.getContext().getExecutorService().submit(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        while(!Thread.currentThread().isInterrupted()) {
-                            dispatchEvents();
-                        }
-                    } catch (InterruptedException ex) {
-                        assert true;
-                    } catch (IOException ex) {
-                        addError("I/O error while sending logs.", ex);
-                    }
-                    addInfo("shutting down");
-                }
-            });
         } catch (Exception ex) {
             addError("Connect server error", ex);
         }
@@ -78,42 +53,28 @@ public class WebSocketAppender extends AppenderBase<ILoggingEvent> {
     public void stop() {
         if (!isStarted()) return;
         CloseUtil.closeQuietly(session);
-        task.cancel(true);
         super.stop();
     }
 
     @Override
     protected void append(ILoggingEvent event) {
         if (event == null || !isStarted()) return;
-        addInfo("append: " + event);
-        queue.offer(event);
-    }
 
-    private void dispatchEvents() throws InterruptedException, IOException {
-        RemoteEndpoint.Basic remote = session.getBasicRemote();
         ByteArrayOutputStream baos = new ByteArrayOutputStream(4096);
-
-        while (true) {
-            ILoggingEvent event = queue.take();
-            Serializable serEvent = pst.transform(event);
-            baos.reset();
+        RemoteEndpoint.Async remote = session.getAsyncRemote();
+        Serializable serEvent = pst.transform(event);
+        try {
             ObjectOutputStream oos = new ObjectOutputStream(baos);
             oos.writeObject(serEvent);
             oos.flush();
             remote.sendBinary(ByteBuffer.wrap(baos.toByteArray()));
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
         }
-    }
 
-    BlockingQueue<ILoggingEvent> newBlockingQueue(int queueSize) {
-        return queueSize <= 0 ?
-                new SynchronousQueue<ILoggingEvent>() : new ArrayBlockingQueue<ILoggingEvent>(queueSize);
     }
 
     public void setServerUri(String uri) {
         this.serverUri = URI.create(uri);
-    }
-
-    public void setQueueSize(int queueSize) {
-        this.queueSize = queueSize;
     }
 }
